25d9c05674c849e8f0e4653f146920af31773c37
Lamptth <63134842+Lamptth@users.noreply.github.com>
2021-03-13 09:39:37 +0700

    Lam-data

---
+++ SymphonyWebApp/Data/Entities/Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SymphonyWebApp.Data.Entities
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public string Title { get; set; }
+        public string Answer { get; set; }
+    }
+}
