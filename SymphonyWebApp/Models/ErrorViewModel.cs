1247c23fc8491d1a74d9eb60386b2483eee0685d
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2021-03-13 08:13:49 +0700

    new project

---
+++ SymphonyWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SymphonyWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
