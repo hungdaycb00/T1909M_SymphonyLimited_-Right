95e74fcbaf5acbc9f5b4c169c48472404d418051
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2021-03-13 09:17:53 +0700

    add readme.txt

---
+++ SymphonyWebApp/Data/Entities/Course.cs
@@ -0,0 +1,28 @@
+﻿using SymphonyWebApp.Data.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SymphonyWebApp.Data.Entities
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+
+        public string CourseName { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Fee { get; set; }
+
+        public Level level { get; set; }
+        public FeeStatus FeeStatus { get; set; }
+
+        public DateTime TrainingTime { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ SymphonyWebApp/Data/Entities/Enums/FeeStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SymphonyWebApp.Data.Entities.Enums
+{
+    public enum FeeStatus
+    {
+        Paid,
+        Unpaid
+    }
+}
