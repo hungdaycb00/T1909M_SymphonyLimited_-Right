d89b40530db7f529c7ee70e375d0324499435135
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2021-03-16 08:22:02 +0700

    fix update

---
+++ SymphonyWebApp/Data/Configurations/QuestionConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SymphonyWebApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SymphonyWebApp.Data.Configurations
+{
+    public class QuestionConfigurations : IEntityTypeConfiguration<Question>
+    {
+        public void Configure(EntityTypeBuilder<Question> builder)
+        {
+            builder.ToTable("Questions");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Title).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Answer).IsRequired().HasMaxLength(100);
+        }
+    }
+}
