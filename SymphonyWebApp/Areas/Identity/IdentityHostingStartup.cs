29273f95e86f8c729b005133ebae717bd0880d59
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2021-04-02 08:45:41 +0700

    create layout login

---
+++ SymphonyWebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SymphonyWebApp.Data;
+
+[assembly: HostingStartup(typeof(SymphonyWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace SymphonyWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
