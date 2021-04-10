aa25448b69f4390cf955a63c924fe6f7e4f922b4
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2021-04-10 15:57:00 +0700

    create dashboard admin

--- SymphonyWebApp/Controllers/AdminController.cs
+++ SymphonyWebApp/Controllers/AdminController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SymphonyWebApp.Data;
+using SymphonyWebApp.Data.Entities;
+using SymphonyWebApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +13,34 @@ namespace SymphonyWebApp.Controllers
     [Authorize]
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            Teachers = _context.Teacher.ToList();
+            ClassStudies = _context.ClassStudies.ToList();
+            Courses = _context.Courses.ToList();
+            Students = _context.Students.ToList();
+
+            ViewData["Teachers"] = Teachers;
+            ViewData["ClassStudies"] = ClassStudies;
+            ViewData["Courses"] = Courses;
+            ViewData["Students"] = Students;
+
             return View();
         }
+        public void OnGet()
+        {
+            
+        }
+        public IEnumerable<Teacher> Teachers { get; set; }
+        public IEnumerable<ClassStudy> ClassStudies { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Student> Students { get; set; }
+
     }
 }
---
+++ SymphonyWebApp/Models/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SymphonyWebApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SymphonyWebApp.Models
+{
+    public class ViewModel
+    {
+        public IEnumerable<Teacher> Teachers { get; set; }
+        public IEnumerable<ClassStudy> ClassStudies { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Student> Students { get; set; }
+    }
+}
